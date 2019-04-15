7a13891d0636477d5b99c03e04ef4401b5610e8c
AntonZh <zhukov.anton.02@gmail.com>
2019-04-15 16:36:09 +0300

    Dos filter implemented

---
+++ src/main/java/com/es/phoneshop/web/DosFilter.java
@@ -0,0 +1,31 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.filterservice.DosFilterService;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DosFilter implements Filter {
+    private DosFilterService service;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        service = DosFilterService.getInstance();
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (service.isAllowed(servletRequest.getRemoteAddr())) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+        else {
+            ((HttpServletResponse)servletResponse).setStatus(429);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
